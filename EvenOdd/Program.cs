259cf13fbbde4f82c2fa94fff15ddbdcddfc72d3
vidyashreehipparagi <hipparagividyashree14@gmail.com>
2023-06-21 16:16:48 +0530

    Add project files.

---
+++ EvenOdd/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num;
+Console.WriteLine("Enter Number");
+num=Convert.ToInt32(Console.ReadLine());
+
+String result = (num >= 0) ? "Postive" : "Negative";
+Console.WriteLine(result);
